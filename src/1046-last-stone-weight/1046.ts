a9f6e5e6c02a9693220d069ea85682fee18b3db7
Arpit Sheth <shetharp@gmail.com>
2020-08-26 02:22:39 -0400

    Add 1046-last-stone-weight

---
+++ src/1046-last-stone-weight/1046.ts
@@ -0,0 +1,18 @@
+// 1046. Last Stone Weight
+// https://leetcode.com/problems/last-stone-weight
+
+/**
+ * Solve
+ */
+// 🟡 Add the function or class skeleton from LeetCode here.
+ 
+/**
+ * Test
+ */
+describe("1046. Last Stone Weight", () => {
+    it("example 1", () => {
+        // 🟡 Add test case here.
+        const input = 0;
+        expect(input).toBe(0);
+    });
+}); 
