c118bee33fc2e2c1453501f284090b5212ee2a9e
Arpit Sheth <shetharp@gmail.com>
2020-08-26 02:26:36 -0400

    Add sample problems

---
+++ src/876-middle-of-the-linked-list/876.ts
@@ -0,0 +1,18 @@
+// 876. Middle of the Linked List
+// https://leetcode.com/problems/middle-of-the-linked-list
+
+/**
+ * Solve
+ */
+// 🟡 Add the function or class skeleton from LeetCode here.
+ 
+/**
+ * Test
+ */
+describe("876. Middle of the Linked List", () => {
+    it("example 1", () => {
+        // 🟡 Add test case here.
+        const input = 0;
+        expect(input).toBe(0);
+    });
+}); 
