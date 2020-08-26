28c6a62b29ecc62b490552a0a32336bae07225ad
Arpit Sheth <shetharp@gmail.com>
2020-08-26 02:25:46 -0400

    Add sample problems

---
+++ src/122-best-time-to-buy-and-sell-stock-ii/122.ts
@@ -0,0 +1,18 @@
+// 122. Best Time to Buy and Sell Stock II
+// https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii
+
+/**
+ * Solve
+ */
+// 🟡 Add the function or class skeleton from LeetCode here.
+ 
+/**
+ * Test
+ */
+describe("122. Best Time to Buy and Sell Stock II", () => {
+    it("example 1", () => {
+        // 🟡 Add test case here.
+        const input = 0;
+        expect(input).toBe(0);
+    });
+}); 
---
+++ src/547-friend-circles/547.ts
@@ -0,0 +1,18 @@
+// 547. Friend Circles
+// https://leetcode.com/problems/friend-circles
+
+/**
+ * Solve
+ */
+// 🟡 Add the function or class skeleton from LeetCode here.
+ 
+/**
+ * Test
+ */
+describe("547. Friend Circles", () => {
+    it("example 1", () => {
+        // 🟡 Add test case here.
+        const input = 0;
+        expect(input).toBe(0);
+    });
+}); 
