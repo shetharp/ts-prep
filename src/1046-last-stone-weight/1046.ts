3585904585aa35467e494c943591fbd90c781357
Arpit Sheth <shetharp@gmail.com>
2020-08-26 02:28:27 -0400

    Solve 1046-last-stone-weight

--- src/1046-last-stone-weight/1046.ts
+++ src/1046-last-stone-weight/1046.ts
@@ -4,15 +4,33 @@
 /**
  * Solve
  */
-// 🟡 Add the function or class skeleton from LeetCode here.
- 
+function lastStoneWeight(stones: number[]): number {
+    if (stones.length == 0) {
+        return 0;
+    }
+    if (stones.length == 1) {
+        return stones[0];
+    }
+
+    stones.sort((a, b) => a - b);
+    const stone1 = stones.pop() || 0;
+    const stone2 = stones.pop() || 0;
+    const smashedStone = stone1 - stone2;
+    smashedStone > 0 && stones.push(smashedStone);
+    return lastStoneWeight(stones);
+};
+
 /**
  * Test
  */
 describe("1046. Last Stone Weight", () => {
     it("example 1", () => {
-        // 🟡 Add test case here.
-        const input = 0;
-        expect(input).toBe(0);
+        const input = [2, 7, 4, 1, 8, 1];
+        expect(lastStoneWeight(input)).toBe(1);
+    });
+
+    it("example 2", () => {
+        const input = [2, 3, 1];
+        expect(lastStoneWeight(input)).toBe(0);
     });
 }); 
